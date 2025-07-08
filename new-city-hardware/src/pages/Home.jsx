60e5cc3bdd7da6c057388c436cb58feb0e1d4e90
RDinujaya4 <136170467+RDinujaya4@users.noreply.github.com>
2025-07-08 14:48:00 +0530

    Project setup successfuly

---
+++ new-city-hardware/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-500">Home Page</h1>
+      <p className="mt-4 text-lg">Welcome to Hardware Shop!</p>
+    </div>
+  );
+}
+export default Home;
