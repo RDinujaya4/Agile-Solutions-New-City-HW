5d2ee2a741b699b199b7456b27fc89fd1676a80f
Dahami <136171018+Dahami0629@users.noreply.github.com>
2025-07-10 21:57:53 +0530

    Cart Page V1

--- new-city-hardware/src/App.jsx
+++ new-city-hardware/src/App.jsx
@@ -5,6 +5,7 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Products from './pages/Product';
 import Contact from './pages/Contact';
+import Cart from './pages/Cart';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/products" element={<Products />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
       <Footer />
     </Router>
