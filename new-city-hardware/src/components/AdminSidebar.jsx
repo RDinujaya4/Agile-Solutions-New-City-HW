dda6630cdc3ecf189ca377031196a8a4f270a866
RDinujaya4 <136170467+RDinujaya4@users.noreply.github.com>
2025-07-19 17:08:30 +0530

    admin side bar updated

--- new-city-hardware/src/components/AdminSidebar.jsx
+++ new-city-hardware/src/components/AdminSidebar.jsx
@@ -13,6 +13,7 @@ import { auth } from '../firebase';
 import { useEffect, useState } from 'react';
 import toast from 'react-hot-toast';
 import Swal from 'sweetalert2';
+import defaultProfile from '../assets/admin-image.png';
 
 export default function AdminSidebar() {
   const navigate = useNavigate();
@@ -46,9 +47,16 @@ export default function AdminSidebar() {
 
   return (
     <aside className="w-64 bg-white border-r border-gray-300 p-6 space-y-6">
-      <div className="text-center">
-        <p className="font-bold">Administrator</p>
-        <p className="text-xs text-gray-500">{adminEmail || 'Loading...'}</p>
+      <div className="flex items-center gap-3">
+        <img
+          src={defaultProfile}
+          alt="Admin Profile"
+          className="w-10 h-10 rounded-full object-cover border"
+        />
+        <div>
+          <p className="font-bold">Administrator</p>
+          <p className="text-xs text-gray-500">{adminEmail || 'Loading...'}</p>
+        </div>
       </div>
 
       <nav className="space-y-2 text-sm font-medium">
